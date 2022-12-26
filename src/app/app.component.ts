7e2d1ed369238c2cdcef941cd7e05785d2f8fde4
Sai <Sai@192.168.1.47>
2022-12-26 16:05:51 +0530

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,7 +44,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   /**
    * Constructor
-   *
+   *testing
    * @param {DOCUMENT} document
    * @param {Title} _title
    * @param {Renderer2} _renderer
