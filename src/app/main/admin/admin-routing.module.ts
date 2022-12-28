784e13156a517c28af81126d41d2d204402525d9
mnmay <mnmay@192.168.1.39>
2022-12-28 16:23:10 +0530

    mayur

---
+++ src/app/main/admin/admin-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DemoComponent } from './organization/demo/demo.component';
+
+
+const routes: Routes = [
+  {path: 'demo', component:DemoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminRoutingModule { }
